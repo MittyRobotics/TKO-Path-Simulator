746d694207b07de91db9c2573d88983d4052f2ad
AndyLi23 <andyliqy@gmail.com>
2022-02-20 17:38:46 -0800

    grid :D

--- core/src/com/github/mittyrobotics/tools/Renderer2D.java
+++ core/src/com/github/mittyrobotics/tools/Renderer2D.java
@@ -4,7 +4,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.github.mittyrobotics.PathSim;
 
@@ -13,15 +16,14 @@ public class Renderer2D {
     public static CamController2D camController;
     public boolean loading;
     public Texture field, title1, title2;
-    public SpriteBatch batch;
+    public SpriteBatch batch, fontBatch;
 
-    public double fieldWidth, fieldHeight;
-    public int width, height;
-    public double zoom, centerx, centery, xc, yc;
-
-    public int right;
+    public double fieldWidth, fieldHeight, inch, zoom, centerx, centery, xc, yc;
+    public int width, height, right;
 
     public ShapeRenderer shapeRenderer;
+    public BitmapFont font;
+    public ShaderProgram fontShader;
 
     public Renderer2D() {
 
@@ -30,6 +32,7 @@ public class Renderer2D {
 
         loading = true;
         batch = new SpriteBatch();
+        fontBatch = new SpriteBatch();
         xc = 0;
         yc = 0;
         zoom = 0.15;
@@ -39,6 +42,17 @@ public class Renderer2D {
         shapeRenderer = new ShapeRenderer();
 
         right = Gdx.graphics.getWidth() - PathSim.RIGHT_WIDTH;
+
+
+        Texture texture = new Texture(Gdx.files.internal("font/futura12.png"));
+        texture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+
+        font = new BitmapFont(Gdx.files.internal("font/futura12.fnt"), new TextureRegion(texture), false);
+
+        fontShader = new ShaderProgram(Gdx.files.internal("font/font.vert"), Gdx.files.internal("font/font.frag"));
+        if (!fontShader.isCompiled()) {
+            Gdx.app.error("fontShader", "compilation failed:\n" + fontShader.getLog());
+        }
 
     }
 
@@ -68,24 +82,112 @@ public class Renderer2D {
             double y = yc + (height - zoom * fieldHeight)/2;
             centerx = x + (fieldWidth * zoom) / 2;
             centery = y + (fieldHeight * zoom) / 2;
+            inch = fieldWidth * zoom / 864;
 
 
             batch.begin();
             batch.draw(field, (int) x, (int) y, (int) (fieldWidth * zoom), (int) (fieldHeight * zoom));
             batch.end();
 
+            fontBatch.begin();
+            fontBatch.setShader(fontShader);
+            if(zoom < 0.22) {
+                drawText(font, fontBatch, centerx, centery, width, height, 48 * inch, 4);
+            } else if (zoom < 0.4) {
+                drawText(font, fontBatch, centerx, centery, width, height, 24 * inch, 2);
+            } else if (zoom < 0.7) {
+                drawText(font, fontBatch, centerx, centery, width, height, 12 * inch, 1);
+            } else {
+                drawText(font, fontBatch, centerx, centery, width, height, 12 * inch, 1);
+            }
+            fontBatch.end();
+
+            Gdx.gl.glEnable(GL20.GL_BLEND);
+            Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
             shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+
+            shapeRenderer.setColor(0.9f, 0.9f, 0.9f, 1f);
+            shapeRenderer.rect(0, (int) centery-1, width, 1);
+            shapeRenderer.rect((int) centerx-1, 0, 1, height);
+
+            if(zoom < 0.22) {
+                drawGrid(shapeRenderer, centerx, centery, width, height, 24 * inch, 0.3f);
+            } else if (zoom < 0.4) {
+                drawGrid(shapeRenderer, centerx, centery, width, height, 12 * inch, 0.3f);
+            } else if (zoom < 0.7) {
+                drawGrid(shapeRenderer, centerx, centery, width, height, 12 * inch, 0.3f);
+                drawGrid(shapeRenderer, centerx, centery, width, height, 6 * inch, 0.2f);
+            } else {
+                drawGrid(shapeRenderer, centerx, centery, width, height, 1 * inch, 0.1f);
+                drawGrid(shapeRenderer, centerx, centery, width, height, 6 * inch, 0.2f);
+                drawGrid(shapeRenderer, centerx, centery, width, height, 12 * inch, 0.3f);
+            }
+
 
             shapeRenderer.setColor(0.15f, 0.15f, 0.15f, 1f);
             shapeRenderer.rect(width, 0, PathSim.RIGHT_WIDTH, height);
 
             shapeRenderer.end();
 
+            Gdx.gl.glDisable(GL20.GL_BLEND);
+
             batch.begin();
             batch.draw(title1, right + 50, Gdx.graphics.getHeight() - 150, 200, (int) (200. * title1.getHeight() / title1.getWidth()));
             batch.draw(title2, right + 40, Gdx.graphics.getHeight() - 210, 220, (int) (220. * title2.getHeight() / title2.getWidth()));
             batch.end();
 
+        }
+    }
+
+    public void drawGrid(ShapeRenderer renderer, double centerx, double centery, int width, int height, double increment, float transparency) {
+        renderer.setColor(0.78f, 0.78f, 0.78f, transparency);
+
+        for(double i = centerx + increment; i <= width; i += increment) {
+            renderer.rect((int) (i-0.5), 0, 1, height);
+        }
+
+        for(double i = centerx - increment; i >= 0; i -= increment) {
+            renderer.rect((int) (i-0.5), 0, 1, height);
+        }
+
+        for(double i = centery + increment; i <= height; i += increment) {
+            renderer.rect(0, (int) (i-0.5), width, 1);
+        }
+
+        for(double i = centery - increment; i >= 0; i -= increment) {
+            renderer.rect(0, (int) (i-0.5), width, 1);
+        }
+    }
+
+    public void drawText(BitmapFont font, SpriteBatch batch, double centerx, double centery, int width, int height, double increment, int t) {
+        int cur = 0;
+
+        double centerynew = Math.max(Math.min(centery, height), 20);
+        double centerxnew = Math.max(Math.min(centerx, width-2), 28);
+
+        for(double i = centerx + increment; i <= width; i += increment) {
+            cur += t;
+            font.draw(batch, cur+"", (int) (i-4-4*((cur+"").length()-1)), (int) centerynew - 5);
+        }
+
+        cur = 0;
+        for(double i = centerx - increment; i >= 0; i -= increment) {
+            cur -= t;
+            font.draw(batch, cur+"", (int) (i-4-4*((cur+"").length()-1)), (int) centerynew - 5);
+        }
+
+        cur = 0;
+        for(double i = centery + increment; i <= height; i += increment) {
+            cur += t;
+            String temp = " ".repeat(3-(cur+"").length());
+            font.draw(batch, temp + cur, (int) centerxnew - 26, (int) (i+6));
+        }
+
+        cur = 0;
+        for(double i = centery - increment; i >= 0; i -= increment) {
+            cur -= t;
+            String temp = " ".repeat(3-(cur+"").length());
+            font.draw(batch, temp + cur, (int) centerxnew - 26, (int) (i+6));
         }
     }
 
